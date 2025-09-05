26f084e0365cf149f3225397952261faec8841b3
q01020304 <2502946053@qq.com>
2025-09-05 16:57:53 +0800

    feat:增加list接口

--- spring-boot-quickstart/src/main/java/top/mqxu/boot/week1/controller/HelloController.java
+++ spring-boot-quickstart/src/main/java/top/mqxu/boot/week1/controller/HelloController.java
@@ -3,6 +3,8 @@ package top.mqxu.boot.week1.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @RestController
 public class HelloController {
@@ -10,4 +12,10 @@ public class HelloController {
     public String hello() {
         return "Hello World!";
     }
+
+    @GetMapping("/list")
+        public List<String> list(){
+            return List.of("1","2","3");
+        }
+
 }
